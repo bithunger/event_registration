3d13efb83fc1a160b1a3f7932b9a6572f732bccc
bithunger <123397821+bithunger@users.noreply.github.com>
2023-11-30 13:51:29 +0600

    add search functionality

--- events/views.py
+++ events/views.py
@@ -4,8 +4,14 @@ from user_dashboard.models import UserEvent
 
 
 def event_list(request):
-    events = Event.objects.all()
+    if request.GET:
+        query = request.GET.get("search")
+        if query:
+            events = Event.objects.filter(title__icontains=query)
+        else:
+            events = Event.objects.none()
+    else:
+        events = Event.objects.all()
+        
     return render(request, 'events/event_list.html', {'events': events})
-
-
 
